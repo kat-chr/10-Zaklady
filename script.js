8d199d461f3d4ce6b45fed23af1671e57f0188a9
Kateřina Chroncová <k.chroncova@gmail.com>
2022-08-02 09:29:08 +0200

    trials

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("JavaScript funguje!");
+console.log(  Math.floor(Math.random()*100)  );
+
+/*
+let mojeBarvaVlasu = prompt( "Zadejte svoji barvu vlasu" );
+*/
+
+function pozdrav() {
+    console.log("Ahoj");
+}
+
+let rok = prompt("zadej svuj rok narozeni");
+let dnesniDatum = new Date();
+pozdrav();
+let stari = dnesniDatum.getFullYear() - rok; 
+console.log("Uzivatel ma " + stari + " let");
+
+
+
